6472552ca5061de6663c1941537fc7a5b860d460
Alex Curley <=>
2024-10-24 10:39:12 -0400

    Add Triangle struct

---
+++ triangle.go
@@ -0,0 +1,23 @@
+package mtk
+
+// Three dimensional Cartesian triangle
+type Triangle [3]Vector3
+
+// Get the normal vector (not necessarily a unit vector)
+func (t Triangle) Normal() Vector3 {
+	pq := t[1] - t[0]
+	pr := t[2] - t[0]
+	return pq.Cross(pr)
+}
+
+// Get the unit normal vector
+func (t Triangle) UnitNormal() Vector3 {
+	return t.Normal().Unit()
+}
+
+// Get the area
+func (t Triangle) Area() float64 {
+	pq := t[1] - t[0]
+	pr := t[2] - t[0]
+	return 0.5 * pq.Cross(pr).Mag()
+}
