6e14fb41904f24088b6bc7f48fa63766e15fad01
Alex Curley <=>
2024-11-02 09:14:53 -0400

    Add interface definitions and geometric tolerance

---
+++ collision.go
@@ -0,0 +1,26 @@
+package mtk
+
+const (
+	// Geometric tolerance
+	EPSILON float64 = 1e-8
+)
+
+// Interface for an AABB intersection test
+type IntersectsAABB interface {
+	IntersectsAABB(AABB) bool
+}
+
+// Interface for a Ray intersection test
+type IntersectsRay interface {
+	IntersectsRay(Ray) bool
+}
+
+// Interface for a Triangle intersection test
+type IntersectsTriangle interface {
+	IntersectsTriangle(Triangle)
+}
+
+// Interface for a Vector3 intersection test
+type IntersectsVector3 interface {
+	IntersectsVector3(Vector3) bool
+}
