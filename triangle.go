6100a4982c6e94abcb1a0c110daab2ea55a80088
Alex Curley <=>
2024-11-03 12:57:09 -0500

    Add Triangle/AABB intersection tests

--- triangle.go
+++ triangle.go
@@ -1,5 +1,9 @@
 package mtk
 
+import (
+	"math"
+)
+
 // Three dimensional Cartesian triangle
 type Triangle [3]Vector3
 
@@ -29,6 +33,155 @@ func (t Triangle) Area() float64 {
 
 // Check for an intersection with an AABB
 func (t Triangle) IntersectsAABB(a AABB) bool {
-	// TODO: implement
-	panic("not implemented")
+	// Shift the system such that the AABB is centered at the origin
+	halfSize := a.HalfSize()
+	center := a.Center()
+	v0 := t[0].Sub(center)
+	v1 := t[1].Sub(center)
+	v2 := t[2].Sub(center)
+
+	// Compute the triangle edges
+	e0 := v1.Sub(v0)
+	e1 := v2.Sub(v1)
+	e2 := v0.Sub(v2)
+
+	// Bullet #1 - Test the AABB against the minimum AABB of the triangle
+	for i := 0; i < 3; i++ {
+		vMin := min(v0[i], v1[i], v2[i])
+		vMax := max(v0[i], v1[i], v2[i])
+
+		if vMin > halfSize[i] || vMax < -halfSize[i] {
+			return false
+		}
+	}
+
+	// Bullet #2 - Test the triangle plane against the AABB
+	normal := e0.Cross(e1)
+
+	if !planeBoxOverlap(normal, v0, halfSize) {
+		return false
+	}
+
+	// Bullet #3 - 9 tests
+	var fex, fey, fez float64
+	fex = math.Abs(e0[0])
+	fey = math.Abs(e0[1])
+	fez = math.Abs(e0[2])
+
+	if !axisTestX01(e0[2], e0[1], fez, fey, v0, v2, halfSize) {
+		return false
+	}
+
+	if !axisTestY02(e0[2], e0[0], fez, fex, v0, v2, halfSize) {
+		return false
+	}
+
+	if !axisTestZ12(e0[1], e0[0], fey, fex, v1, v2, halfSize) {
+		return false
+	}
+
+	fex = math.Abs(e1[0])
+	fey = math.Abs(e1[1])
+	fez = math.Abs(e1[2])
+
+	if !axisTestX01(e1[2], e1[1], fez, fey, v0, v2, halfSize) {
+		return false
+	}
+
+	if !axisTestY02(e1[2], e1[0], fez, fex, v0, v2, halfSize) {
+		return false
+	}
+
+	if !axisTestZ0(e1[1], e1[0], fey, fex, v0, v1, halfSize) {
+		return false
+	}
+
+	fex = math.Abs(e2[0])
+	fey = math.Abs(e2[1])
+	fez = math.Abs(e2[2])
+
+	if !axisTestX2(e2[2], e2[1], fez, fey, v0, v1, halfSize) {
+		return false
+	}
+
+	if !axisTestY1(e2[2], e2[0], fez, fex, v0, v1, halfSize) {
+		return false
+	}
+
+	if !axisTestZ12(e2[1], e2[0], fey, fex, v1, v2, halfSize) {
+		return false
+	}
+
+	return true
+}
+
+func axisTestX01(a, b, fa, fb float64, v0, v2, h Vector3) bool {
+	p0 := a*v0[1] - b*v0[2]
+	p2 := a*v2[1] - b*v2[2]
+	pMin := min(p0, p2)
+	pMax := max(p0, p2)
+	rad := fa*h[1] + fb*h[2]
+	return !(pMin > rad || pMax < -rad)
+}
+
+func axisTestX2(a, b, fa, fb float64, v0, v1, h Vector3) bool {
+	p0 := a*v0[1] - b*v0[2]
+	p1 := a*v1[1] - b*v1[2]
+	pMin := min(p0, p1)
+	pMax := max(p0, p1)
+	rad := fa*h[1] + fb*h[2]
+	return !(pMin > rad || pMax < -rad)
+}
+
+func axisTestY02(a, b, fa, fb float64, v0, v2, h Vector3) bool {
+	p0 := -a*v0[0] + b*v0[2]
+	p2 := -a*v2[0] + b*v2[2]
+	pMin := min(p0, p2)
+	pMax := max(p0, p2)
+	rad := fa*h[0] + fb*h[2]
+	return !(pMin > rad || pMax < -rad)
+}
+
+func axisTestY1(a, b, fa, fb float64, v0, v1, h Vector3) bool {
+	p0 := -a*v0[0] + b*v0[2]
+	p1 := -a*v1[0] + b*v1[2]
+	pMin := min(p0, p1)
+	pMax := max(p0, p1)
+	rad := fa*h[0] + fb*h[2]
+	return !(pMin > rad || pMax < -rad)
+}
+
+func axisTestZ12(a, b, fa, fb float64, v1, v2, h Vector3) bool {
+	p1 := a*v1[0] - b*v1[1]
+	p2 := a*v2[0] - b*v2[1]
+	pMin := min(p1, p2)
+	pMax := max(p1, p2)
+	rad := fa*h[0] + fb*h[1]
+	return !(pMin > rad || pMax < -rad)
+}
+
+func axisTestZ0(a, b, fa, fb float64, v0, v1, h Vector3) bool {
+	p0 := a*v0[0] - b*v0[1]
+	p1 := a*v1[0] - b*v1[1]
+	pMin := min(p0, p1)
+	pMax := max(p0, p1)
+	rad := fa*h[0] + fb*h[1]
+	return !(pMin > rad || pMax < -rad)
+}
+
+func planeBoxOverlap(normal, vertex, boxMax Vector3) bool {
+	vMin := Vector3{0, 0, 0}
+	vMax := Vector3{0, 0, 0}
+
+	for i := 0; i < 3; i++ {
+		if normal[i] > 0 {
+			vMin[i] = -boxMax[i] - vertex[i]
+			vMax[i] = boxMax[i] - vertex[i]
+		} else {
+			vMin[i] = boxMax[i] - vertex[i]
+			vMax[i] = -boxMax[i] - vertex[i]
+		}
+	}
+
+	return normal.Dot(vMin) <= 0 && normal.Dot(vMax) >= 0
 }
---
+++ triangle_test.go
@@ -0,0 +1,232 @@
+package mtk
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test an AABB/Triangle intersection for a fully contained triangle
+func TestTriangleIntersectsAABBHitContained(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{0.1, 0.1, 0.1},
+		Vector3{0.1, 0.1, 0.3},
+		Vector3{0.1, 0.3, 0.1},
+	}
+
+	assert.True(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection for a triangle crossing a face
+func TestTriangleIntersectsAABBHitCrossFace(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{0.5, 0.5, 0.5},
+		Vector3{1.25, 1.75, 0.5},
+		Vector3{1.25, 0.25, 0.5},
+	}
+
+	assert.True(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection for a triangle crossing an edge
+func TestTriangleIntersectsAABBHitCrossEdge(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{0.25, -0.25, 0.5},
+		Vector3{1.25, 0.75, 0.5},
+		Vector3{1.25, -0.25, 0.5},
+	}
+
+	assert.True(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection for a triangle crossing all four faces
+func TestTriangleIntersectsAABBHitCrossFull(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{-2, -1, 0.5},
+		Vector3{1.5, 3, 0.5},
+		Vector3{1.5, -1, 0.5},
+	}
+
+	assert.True(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss bounds test
+func TestTriangleIntersectsAABBMissAABB(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{0, 0, 2},
+		Vector3{1, 0, 2},
+		Vector3{1, 1, 2},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss plane test
+func TestTriangleIntersectsAABBMissPlane(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{1.1, 1.1, 0.9},
+		Vector3{0.5, 0.8, 1.5},
+		Vector3{0.9, 1.1, 0.9},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss e0 x01 test
+func TestTriangleIntersectsAABBMissE0X01(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{0.5, 1.1, 0.9},
+		Vector3{0.5, 0.8, 1.5},
+		Vector3{0.5, 1.3, 1.2},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss e0 y02 test
+func TestTriangleIntersectsAABBMissE0Y02(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{1.1, 0.5, 0.9},
+		Vector3{0.8, 0.5, 1.5},
+		Vector3{1.3, 0.5, 1.2},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss e0 z12 test
+func TestTriangleIntersectsAABBMissE0Z12(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{1.1, 0.9, 0.5},
+		Vector3{0.8, 1.5, 0.5},
+		Vector3{1.3, 1.2, 0.5},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss e1 x01 test
+func TestTriangleIntersectsAABBMissE1X01(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{0.5, 1.3, 1.2},
+		Vector3{0.5, 1.1, 0.9},
+		Vector3{0.5, 0.8, 1.5},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss e1 y02 test
+func TestTriangleIntersectsAABBMissE1Y02(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{1.3, 0.5, 1.2},
+		Vector3{1.1, 0.5, 0.9},
+		Vector3{0.8, 0.5, 1.5},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss e1 z0 test
+func TestTriangleIntersectsAABBMissE1Z0(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{1.3, 1.2, 0.5},
+		Vector3{1.1, 0.9, 0.5},
+		Vector3{0.8, 1.5, 0.5},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss e2 x2 test
+func TestTriangleIntersectsAABBMissE2X2(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{0.5, 0.8, 1.5},
+		Vector3{0.5, 1.3, 1.2},
+		Vector3{0.5, 1.1, 0.9},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss e2 y1 test
+func TestTriangleIntersectsAABBMissE2Y1(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{0.8, 0.5, 1.5},
+		Vector3{1.3, 0.5, 1.2},
+		Vector3{1.1, 0.5, 0.9},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
+
+// Test an AABB/Triangle intersection miss e2 z12 test
+func TestTriangleIntersectsAABBMissE2Z12(t *testing.T) {
+	aabb := AABB{
+		Min: Vector3{0, 0, 0},
+		Max: Vector3{1, 1, 1},
+	}
+	triangle := Triangle{
+		Vector3{0.8, 1.5, 0.5},
+		Vector3{1.3, 1.2, 0.5},
+		Vector3{1.1, 0.9, 0.5},
+	}
+
+	assert.False(t, triangle.IntersectsAABB(aabb))
+}
