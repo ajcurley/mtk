4429e5ee1f5d2f83dbce10826273f1acd85db6cb
Alex Curley <=>
2024-11-04 07:33:22 -0500

    Remove profiling file

--- cmd/main.go
+++ cmd/main.go
@@ -1,59 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"runtime/pprof"
-	"time"
-
 	"github.com/ajcurley/mtk"
 )
 
 func main() {
 	path := "/Users/acurley/projects/cfd/geometry/car.obj.gz"
-    mesh, _ := mtk.NewHEMeshFromOBJFile(path)
-    bounds := mesh.GetBounds()
-    octree := mtk.NewOctree(bounds)
+	mesh, _ := mtk.NewHEMeshFromOBJFile(path)
+	bounds := mesh.GetBounds()
+	octree := mtk.NewOctree(bounds)
 
-	file, _ := os.Create("cmd/ray_trace.pprof")
-	defer file.Close()
-	pprof.StartCPUProfile(file)
-	defer pprof.StopCPUProfile()
+	for i := 0; i < mesh.GetNumberOfFaces(); i++ {
+		vertices := mesh.GetFaceVertices(i)
+		face := mtk.NewTriangle(
+			mesh.GetVertex(vertices[0]).Origin,
+			mesh.GetVertex(vertices[1]).Origin,
+			mesh.GetVertex(vertices[2]).Origin,
+		)
 
-    buildStart := time.Now()
+		octree.Insert(face)
+	}
 
-    for i := 0; i < mesh.GetNumberOfFaces(); i++ {
-        vertices := mesh.GetFaceVertices(i)
-        face := mtk.NewTriangle(
-            mesh.GetVertex(vertices[0]).Origin,
-            mesh.GetVertex(vertices[1]).Origin,
-            mesh.GetVertex(vertices[2]).Origin,
-        ) 
+	box := mtk.NewAABB(
+		mtk.NewVector3(-2, -0.5, 0.5),
+		mtk.NewVector3(0.5, 0.5, 0.5),
+	)
 
-        octree.Insert(face)
-    }   
-
-    buildTime := time.Now().Sub(buildStart).Milliseconds()
-    fmt.Printf("Octree indexed in %dms\n", buildTime)
-
-	fmt.Println(mesh.GetNumberOfFaces())
-    queries := make([]mtk.IntersectsAABB, 100000)
-
-    for i := 0; i < len(queries); i++ {
-        face := mesh.GetFace(i)
-        halfEdge := mesh.GetHalfEdge(face.HalfEdge)
-        normal := mesh.GetFaceNormal(i).Unit()
-
-        queries[i] = mtk.Ray{
-            Origin:    mesh.GetVertex(halfEdge.Origin).Origin.Add(normal.MulScalar(0.001)),
-            Direction: normal,
-        }   
-    }   
-
-    queryStart := time.Now()
-
-   	octree.QueryMany(queries)
-
-    queryTime := time.Now().Sub(queryStart).Milliseconds()
-    fmt.Printf("Octree queried in %dms\n", queryTime)
+	faces := octree.Query(box)
+	subset, _ := mesh.ExtractFaces(faces)
+	subset.ExportOBJFile("/Users/acurley/Desktop/clipped.obj")
 }
